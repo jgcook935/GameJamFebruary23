fc47d67329c0c6936061559f33c9275c9df297ee
James Cook <james@trainerroad.com>
2023-02-17 10:55:33 -0800

    add some camera shake stuff

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -10,6 +10,8 @@ public class AudioManager : MonoBehaviour
     private void Awake()
     {
         source.clip = overWorldTheme;
+        // source.time
+        // make sure to save the audio clip's time and be able to go back to where we were before
         source.Play();
     }
 
