05daee1d6e6807568fef821b72b4a4495891bbe3
Bryce <bryce@trainerroad.com>
2023-03-09 21:31:35 -0800

    more slider updates

--- Assets/Scripts/AbstractSlider.cs
+++ Assets/Scripts/AbstractSlider.cs
@@ -9,22 +9,27 @@ public abstract class AbstractSlider : MonoBehaviour
     private float currentVelocity = 0f;
     private bool init = false;    
 
-    protected void UpdateSlider(Slider HealthSlider, Image hpColorImage)
+    protected void UpdateSlider(Slider slider, Image hpColorImage)
     {
         if(!init)
         {
-            HealthSlider.value = targetHP;
+            SetSliderValue(slider, targetHP);
             init = true;
         }
         else
         {
-            HealthSlider.value = Mathf.SmoothDamp(HealthSlider.value, targetHP, ref currentVelocity, sliderLerpSpeed * Time.deltaTime);
+            SetSliderValue(slider, Mathf.SmoothDamp(slider.value, targetHP, ref currentVelocity, sliderLerpSpeed * Time.deltaTime));
         }
 
-        if((HealthSlider.value * maxHP) < 0.5f * maxHP)
+        if((slider.value * maxHP) < 0.5f * maxHP)
         {
             hpColorImage.color = Color.red;
         } 
         else hpColorImage.color = new Color(29f / 255f, 168f / 255f, 6f / 255f);
+    }
+
+    private void SetSliderValue(Slider slider, float value)
+    {
+        slider.value = Mathf.Max(value, 0f);
     }
 }
